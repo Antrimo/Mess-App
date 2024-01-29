b329a0e69d86d9f191a3ec6430046a40625d962a
Kartikey Srivastava <kartikeyai246@gmail.com>
2024-01-29 23:46:14 +0530

    carousel done

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:carousel_slider/carousel_slider.dart';
 
 import 'package:mess/Resources/theme.dart';
+import 'package:mess/values.dart';
 
 void main() {
   runApp(const MyApp());
@@ -28,22 +30,26 @@ class _MyAppState extends State<MyApp> {
             const Text(
               "Today's Menu",
               style: TextStyle(
-                  color: Colors.white,
-                  fontSize: 35,
-                  fontWeight: FontWeight.bold,
-                  fontStyle: FontStyle.italic),
+                color: Colors.white,
+                fontSize: 35,
+                fontWeight: FontWeight.bold,
+                fontStyle: FontStyle.italic,
+              ),
             ),
             const SizedBox(
               height: 30,
             ),
-            Center(
-              child: Container(
+            CarouselSlider(
+              options: CarouselOptions(
                 height: 500.0,
-                width: 300.0,
-                decoration: const BoxDecoration(
-                    color: Colors.white,
-                    borderRadius: BorderRadius.all(Radius.circular(50))),
+                enlargeCenterPage: true,
+                enableInfiniteScroll: false,
               ),
+              items: [
+                buildMenuCard("Breakfast", breakfastMenu),
+                buildMenuCard("Lunch", lunchMenu),
+                buildMenuCard("Dinner", dinnerMenu),
+              ],
             ),
             const SizedBox(
               height: 40,
@@ -52,24 +58,51 @@ class _MyAppState extends State<MyApp> {
               height: 60,
               width: 200,
               decoration: const BoxDecoration(
-                  color: Colors.black,
-                  borderRadius: BorderRadius.all(Radius.circular(20))),
-            )
+                color: Colors.black,
+                borderRadius: BorderRadius.all(Radius.circular(20)),
+              ),
+            ),
           ],
         ),
-        // bottomNavigationBar: const Padding(
-        //   padding: EdgeInsets.all(50.0),
-        //   child: GNav(
-        //       // tabBorderRadius: 15,
-        //       backgroundColor: Colors.black,
-        //       activeColor: Colors.white,
-        //       color: Colors.white,
-        //       // padding: EdgeInsets.symmetric(horizontal: 20, vertical: 5),
-        //       tabs: [
-        //         GButton(icon: Icons.home),
-        //         GButton(icon: Icons.lock_clock),
-        //       ]),
-        // ),
+      ),
+    );
+  }
+
+  Widget buildMenuCard(String mealType, List<String> menuItems) {
+    return Center(
+      child: Container(
+        width: 300.0,
+        decoration: const BoxDecoration(
+          color: Colors.white,
+          borderRadius: BorderRadius.all(Radius.circular(50)),
+        ),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text(
+              mealType,
+              style: const TextStyle(
+                color: Colors.black,
+                fontSize: 24,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+            const SizedBox(height: 10),
+            Column(
+              children: menuItems
+                  .map((item) => ListTile(
+                        title: Text(
+                          item,
+                          style: const TextStyle(
+                            color: Colors.black,
+                            fontSize: 16,
+                          ),
+                        ),
+                      ))
+                  .toList(),
+            ),
+          ],
+        ),
       ),
     );
   }
---
+++ lib/values.dart
@@ -0,0 +1,14 @@
+List<String> breakfastMenu = ["Scrambled Eggs", "Bacon", "Toast", "Coffee"];
+
+List<String> lunchMenu = [
+  "Grilled Chicken Salad",
+  "Vegetable Soup",
+  "Brown Rice",
+  "Iced Tea"
+];
+List<String> dinnerMenu = [
+  "Steak",
+  "Mashed Potatoes",
+  "Green Beans",
+  "Red Wine"
+];
